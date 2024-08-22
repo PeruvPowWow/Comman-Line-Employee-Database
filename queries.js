7daea30c310d8667d2c30c0d080fc65b629d13d1
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-08-22 02:45:02 -0600

    added some more content to queries.js

--- queries.js
+++ queries.js
@@ -1,8 +1,29 @@
 const pool = require('./db');
 
 const getAllDepartments = async () => {
+try {
     const result = await pool.query('SELECT * FROM department');
     return result.rows;
+} catch (err) {
+    console.error('Error fetching departments:', err);
+};
+
+const getAllRoles = async () => {
+    try {
+        const result = await pool.query('SELECT * FROM role');
+        return result.rows;
+    } catch (err) {
+        console.error('Error fetching roles:', err);
+    }
+};
+
+const getAllEmployees = async () => {
+    try {
+        const result = await pool.query('SELECT * FROM employee');
+        return result.rows;
+    } catch (err) {
+        console.error('Error fetching employees:', err);
+    }
 };
 
 module.exports = {
